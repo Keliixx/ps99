adfbf564a0e2d9ee9875c534e79c5586bfdfae7b
Keliixx <126080257+Keliixx@users.noreply.github.com>
2024-01-03 03:29:16 +0700

    Update boothsniper.lua

--- boothsniper.lua
+++ boothsniper.lua
@@ -238,15 +238,52 @@ function SendInfo1()
 
 end
 
-task.spawn(function()
-    while task.wait(1080) do
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Keliixx/ps99/main/serverhop.lua"))()
-        SendInfo1()
+local function jumpToServer() 
+    local sfUrl = "https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=%s&limit=%s&excludeFullGames=true" 
+    local req = request({ Url = string.format(sfUrl, 15502339080, "Desc", 100) }) 
+    local body = http:JSONDecode(req.Body) 
+    local deep = math.random(1, 3)
+    if deep > 1 then 
+        for i = 1, deep, 1 do 
+             req = request({ Url = string.format(sfUrl .. "&cursor=" .. body.nextPageCursor, 15502339080, "Desc", 100) }) 
+             body = http:JSONDecode(req.Body) 
+             task.wait(0.1)
+        end 
+    end 
+    local servers = {} 
+    if body and body.data then 
+        for i, v in next, body.data do 
+            if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers and v.id ~= game.JobId then
+                table.insert(servers, v.id)
+            end
+        end
     end
-end)
+    local randomCount = #servers
+    if not randomCount then
+       randomCount = 2
+    end
+    ts:TeleportToPlaceInstance(15502339080, servers[math.random(1, randomCount)], game:GetService("Players").LocalPlayer) 
+end
 
-task.spawn(function()
-    while task.wait() do
-        game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(123,123,123)
+Players.PlayerRemoving:Connect(function(player)
+    PlayerInServer = #getPlayers
+    if PlayerInServer < 25 then
+	SendInfo1()
+        jumpToServer()
     end
-end)
+end) 
+
+Players.PlayerAdded:Connect(function(player)
+    for i = 1,#alts do
+        if player.Name == alts[i] and alts[i] ~= Players.LocalPlayer.Name then
+            jumpToServer()
+        end
+    end
+end) 
+
+while task.wait(1) do
+    if math.floor(os.clock() - osclock) >= math.random(900, 1200) then
+	SendInfo2()
+        jumpToServer()
+    end
+end
