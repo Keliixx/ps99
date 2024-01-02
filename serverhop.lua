588fe73c52914d7baf2536ea41f6f92363dd2c18
Keliixx <126080257+Keliixx@users.noreply.github.com>
2024-01-02 17:37:56 +0700

    Update serverhop.lua

--- serverhop.lua
+++ serverhop.lua
@@ -1,28 +1,24 @@
-local function jumpToServer() 
-local sfUrl = "https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=%s&limit=%s&excludeFullGames=true" 
-local req = request({ Url = string.format(sfUrl, 15502339080, "Desc", 100) }) 
-local body = game:GetService("HttpService"):JSONDecode(req.Body) 
-local deep = math.random(1, 3)
-if deep > 1 then 
-    for i = 1, deep, 1 do 
-        req = request({ Url = string.format(sfUrl .. "&cursor=" .. body.nextPageCursor, 15502339080, "Desc", 100) }) 
-        body = game:GetService("HttpService"):JSONDecode(req.Body) 
-        task.wait(0.1)
-    end 
-end 
-local servers = {} 
-if body and body.data then 
-    for i, v in next, body.data do 
-            if type(v) == "table" and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers and v.id ~= game.JobId then
-                table.insert(servers, 1, v.id)
-            end
-        end
-    end
-    local randomCount = #servers
-    if not randomCount then
-        randomCount = 2
-    end
-    game:GetService("TeleportService"):TeleportToPlaceInstance(15502339080, servers[math.random(1, randomCount)], game:GetService("Players").LocalPlayer) 
+task.wait(10)
+local Player = game.Players.LocalPlayer    
+local Http = game:GetService("HttpService")
+local TPS = game:GetService("TeleportService")
+local Api = "https://games.roblox.com/v1/games/"
+
+local _place,_id = game.PlaceId, game.JobId
+local _servers = Api.._place.."/servers/Public?sortOrder=Desc&limit=100"
+function ListServers(cursor)
+   local Raw = game:HttpGet(_servers .. ((cursor and "&cursor="..cursor) or ""))
+   return Http:JSONDecode(Raw)
 end
 
-jumpToServer()
+local Next; repeat
+   local Servers = ListServers(Next)
+   for i,v in next, Servers.data do
+       if v.playing < v.maxPlayers and v.id ~= _id then
+           local s,r = pcall(TPS.TeleportToPlaceInstance,TPS,_place,v.id,Player)
+           if s then break end
+       end
+   end
+   
+   Next = Servers.nextPageCursor
+until not Next
